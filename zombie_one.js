d1c7edb2912fd63b0bd18dc59576d2a51691a3ab
Terry Peppers <peppers@gmail.com>
2014-06-12 07:15:21 -0500

    Slow

--- zombie_one.js
+++ zombie_one.js
@@ -1,12 +1,11 @@
 var Browser = require("zombie");
 var assert = require("assert");
-var url = "http://www.google.com";
+var url = "http://www.yahoo.com";
 
-browser = new Browser();
+browser = new Browser({ debug: true });
 
 Browser.visit(url, function (e, browser) {
-	console.log("I visited " + url);
-	console.log("----------------");
-	console.log("Here's what I see:");
+	console.log(browser.url);
+	console.log(browser.statusCode);
 	console.log(browser.text("title"));
-})
+});
